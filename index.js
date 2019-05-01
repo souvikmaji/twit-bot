b12ca1cc43636066fb8f2129288740a47ec2b873
souvikmaji <souvikmaji94@gmail.com>
2019-05-01 09:25:25 +0530

    gameofthrones

--- index.js
+++ index.js
@@ -1,37 +1,36 @@
 var Twit = require('twit');
-var request=require('request');
+var request = require('request');
 var config = require('./config');
 
 var T = new Twit(config);
 
 twitIt();
-setInterval(twitIt, 1000*60*30);
+setInterval(twitIt, 1000 * 60 * 30);
 
 function twitIt() {
-    request.get('https://got-quotes.herokuapp.com/quotes',response);
+  request.get('https://got-quotes.herokuapp.com/quotes', response);
 
-    function response(err,res,body) {
-      if(err) {
-          console.log('Error getting quote: ', err);
-      }
-      else if(res.statusCode !== 200 ) {
-          console.log('Error proccessing request: ', res);
-      }
-      else {
-          var quote = JSON.parse(body).quote;
-          var char = JSON.parse(body).character.replace(/\s/g,'');
-          var twit = {status: quote + ' #' + char};
-          console.log(char);
-          T.post('statuses/update', twit, twitted);
-      }
+  function response(err, res, body) {
+    if (err) {
+      console.log('Error getting quote: ', err);
+    } else if (res.statusCode !== 200) {
+      console.log('Error proccessing request: ', res);
+    } else {
+      var quote = JSON.parse(body).quote;
+      var char = JSON.parse(body).character.replace(/\s/g, '');
+      var twit = {
+        status: quote + ' #gameofthrones'
+      };
+      console.log(char);
+      T.post('statuses/update', twit, twitted);
     }
+  }
 
-    function twitted(err, data, response) {
-      if(err){
-        console.log('Something went wrong:', err);
-      }
-      else{
-        console.log(data.text);
-      }
+  function twitted(err, data, response) {
+    if (err) {
+      console.log('Something went wrong:', err);
+    } else {
+      console.log(data.text);
     }
+  }
 }
