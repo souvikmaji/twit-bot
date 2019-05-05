72db6d28ca6b6f1877cafac94a7d7e3625fa17fc
souvikmaji <souvikmaji94@gmail.com>
2019-05-05 13:35:38 +0530

    give me some data reddit; twitter here is a new meme

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Twit = require("twit");
-const request = require("request");
+const request = require("request").defaults({ encoding: null, });
 const config = require("./config");
+
+const fs = require("fs");
+const path = require("path");
 
 const T = new Twit(config);
 
@@ -8,7 +11,7 @@ main();
 
 function getContent () {
     const options = {
-        url: "https://got-quotes.herokuapp.com/quotes",
+        url: "https://www.reddit.com/r/freefolk/hot.json",
     };
 
     return new Promise(function (resolve, reject) {
@@ -16,24 +19,83 @@ function getContent () {
             if (err) {
                 reject(err);
             } else {
-                resolve(JSON.parse(body));
+                resolve(parseSubReddit(JSON.parse(body)));
             }
         });
     });
 }
 
-function postTwit (status) {
-    const twit = {
-        status: status,
+function saveImage (url, path) {
+    const options = {
+        url: url,
     };
-
     return new Promise(function (resolve, reject) {
-        T.post("statuses/update", twit, function (err, data, response) {
+        request.get(options, function (err, resp, body) {
             if (err) {
                 reject(err);
             } else {
-                resolve(data.text);
+                fs.writeFile(path, body, function (err) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
             }
+        });
+    });
+}
+
+function parseSubReddit (response) {
+    let posts = response.data.children;
+    for (let post of posts) {
+        if (!post.data.is_self) {
+            let data = {
+                title: post.data.title,
+                url: post.data.url,
+            };
+            return data;
+        }
+    }
+}
+
+function postTwit (content) {
+    return new Promise(function (resolve, reject) {
+        const localname = "downloaded_image";
+        const PATH = path.join(__dirname, localname);
+        let imagePromise = saveImage(content.url, PATH);
+        imagePromise.then(function () {
+            T.postMediaChunked({ file_path: PATH, }, function (err, data, response) {
+                if (err) {
+                    console.log("upload error");
+                    reject(err);
+                } else {
+                    var mediaIdStr = data.media_id_string;
+                    var altText = content.title;
+                    var metaParams = { media_id: mediaIdStr, alt_text: { text: altText, }, };
+
+                    T.post("media/metadata/create", metaParams, function (err, data, response) {
+                        if (!err) {
+                        // now we can reference the media and post a tweet (media will attach to the tweet)
+                            var params = { status: content.title + " #gameofthrones",
+                                media_ids: [ mediaIdStr, ], };
+
+                            T.post("statuses/update", params, function (err, data, response) {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    fs.unlinkSync(PATH);
+                                    resolve(data.text);
+                                }
+                            });
+                        } else {
+                            reject(err);
+                        }
+                    });
+                }
+            });
+        }, function (err) {
+            reject(err);
         });
     });
 }
@@ -41,10 +103,8 @@ function postTwit (status) {
 function main () {
     const contentPromise = getContent(postTwit);
     contentPromise.then(function (content) {
-        const quote = content.quote;
-        const character = content.character.replace(/\s/g, "");
-
-        const twitted = postTwit(quote + " #gameofthrones" + " #" + character);
+        // parse content to twit
+        const twitted = postTwit(content);
         twitted.then(function (status) {
             console.log(status);
         }, function (err) {
