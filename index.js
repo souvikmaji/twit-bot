44fd754788a2b26a39f0f3efe1ccdea4ef171987
souvikmaji <souvikmaji94@gmail.com>
2019-05-25 13:46:21 +0530

    removing nested promises

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function getContent () {
     return new Promise(function (resolve, reject) {
         request.get(options, function (err, resp, body) {
             if (err) {
+                console.log("Error getting content: ");
                 reject(err);
             } else {
                 resolve(parseSubReddit(JSON.parse(body)));
@@ -65,40 +66,33 @@ function postTwit (content) {
     return new Promise(function (resolve, reject) {
         const localname = "downloaded_image";
         const PATH = path.join(__dirname, localname);
-        let imagePromise = saveImage(content.url, PATH);
-        imagePromise.then(function () {
+
+        return saveImage(content.url, PATH).then(function () {
             T.postMediaChunked({ file_path: PATH, }, function (err, data, response) {
                 if (err) {
                     console.log("upload error");
                     reject(err);
                 } else {
                     var mediaIdStr = data.media_id_string;
-                    var altText = content.title;
-                    var metaParams = { media_id: mediaIdStr, alt_text: { text: altText, }, };
+                    var metaParams = { media_id: mediaIdStr, alt_text: { text: content.title, }, };
 
-                    T.post("media/metadata/create", metaParams)
-                        .catch(function (err) {
-                            console.log("error creating media meta data");
-                            reject(err);
-                        })
+                    return T.post("media/metadata/create", metaParams)
                         .then(function () {
                             // now we can reference the media and post a tweet (media will attach to the tweet)
                             var params = { status: content.title + " #gameofthrones",
                                 media_ids: [ mediaIdStr, ], };
 
-                            T.post("statuses/update", params)
-                                .catch(function (err) {
-                                    console.log("error in status update");
-                                    reject(err);
-                                })
-                                .then(function (result) {
-                                    fs.unlinkSync(PATH);
-                                    resolve(result.data.text);
-                                });
+                            return T.post("statuses/update", params);
+                        }).then(function (result) {
+                            fs.unlinkSync(PATH);
+                            resolve(result.data.text);
+                        }).catch(function (err) {
+                            console.log("error in status update");
+                            reject(err);
                         });
                 }
             });
-        }, function (err) {
+        }).catch(function (err) {
             reject(err);
         });
     });
@@ -108,13 +102,10 @@ function main () {
     const contentPromise = getContent(postTwit);
     contentPromise.then(function (content) {
         // parse content to twit
-        const twitted = postTwit(content);
-        twitted.then(function (status) {
-            console.log(status);
-        }, function (err) {
-            console.log("Something went wrong posting the twit: ", err);
-        });
-    }, function (err) {
-        console.log("Error getting content: ", err);
+        return postTwit(content);
+    }).then(function (status) {
+        console.log(status);
+    }).catch(function (err) {
+        console.log(err);
     });
 }
